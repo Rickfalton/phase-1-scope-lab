612be801ab452e53ab6c9715dc729615bfbbad9c
Rickfalton <rickonyach@gmail.com>
2024-07-02 14:19:52 +0300

    scope

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+var bestCustomer;
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'some initial value';
+
+function changeLeastFavoriteCustomer() {
+    // You can't reassign a const variable, so this will throw an error
+    // leastFavoriteCustomer = 'unsuccessfully tries to reassign the least favorite customer';
+    // Instead, you could log an error message or handle this differently:
+    console.error('Cannot reassign the leastFavoriteCustomer');
+}
---
+++ var customerName = "bob";.js
@@ -0,0 +1,14 @@
+var customerName ="bob";
+function upperCaseCustomerName() {
+   customerName = customerName.toUpperCase();
+ }
+ function setBestCustomer() {
+   bestCustomer = 'not bob';
+ }
+ function  overwriteBestCustomer(){
+   bestCustomer = 'maybe bob';
+ } 
+ const leastFavoriteCustomer = 'Assignment to constant variable.';
+ function changeLeastFavoriteCustomer(){
+   leastFavoriteCustomer = 'unsuccessfully tries to reassign the least favorite customer';
+ }
